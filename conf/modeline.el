871195192c0c1bdb9c04587f3b091071edcee365
Illia Danko <illia@aigent.com>
2021-03-24 12:43:36 +0200

    modeline: whitelist Flycheck mode

--- conf/modeline.el
+++ conf/modeline.el
@@ -1,11 +1,17 @@
 ;;; ~/.emacs.d/conf/modeline.el -*- lexical-binding: t; -*-
 
+(defvar modeline-minor-white-list '(flycheck-mode)
+  "The whitelist of the minor modes being shown on the modeline.")
+
 ;;; Turn off minor modes.
 ;; https://emacs.stackexchange.com/questions/3925/hide-list-of-minor-modes-in-mode-line#answer-3928
 (defun purge-minor-modes ()
   (interactive)
   (setq minor-mode-alist
-        (mapcar (lambda (x) (list (car x) ""))
+        (mapcar (lambda (x)
+                  (if (member (car x) modeline-minor-white-list)
+                      x
+                    `(,(car x) "")))
                 minor-mode-alist)))
 
 (add-hook 'after-change-major-mode-hook 'purge-minor-modes)
