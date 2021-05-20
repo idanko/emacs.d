977b50107712b54d31cadc0e7cda554aeb89cc03
Illia Danko <idanko@tuta.io>
2021-05-20 13:28:22 +0300

    flycheck: show errs/warns on margin, add custom mapping

--- cfg/checkers.el
+++ cfg/checkers.el
@@ -2,3 +2,9 @@
 
 ;;; Flycheck.
 (straight-use-package 'flycheck)
+
+(setq flycheck-indication-mode 'left-margin
+      flycheck-highlighting-mode nil)
+
+(evil-global-set-key 'normal (kbd "<localleader> t e") #'flycheck-list-errors)
+
