a239391cc8d598ed91d78a2ed694ddecc057c788
Illia Danko <idanko@tuta.io>
2021-05-24 22:43:25 +0300

    lisp: fix mapping bug

--- cfg/lang/lisp.el
+++ cfg/lang/lisp.el
@@ -14,14 +14,22 @@
 (defun id/clojure-mode-hook ()
   (id/shared-lisp-mode-hook)
   (clj-refactor-mode +1)
-  (evil-global-set-key 'normal (kbd "<localleader> e r") #'cider-eval-defun-at-point)
-  (evil-global-set-key 'normal (kbd "<localleader> e s") #'cider-eval-last-sexp)
-  (evil-global-set-key 'normal (kbd "<localleader> e b") #'cider-eval-buffer)
-  (evil-global-set-key 'normal (kbd "<localleader> c c") #'cider-jack-in)
-  (evil-global-set-key 'normal (kbd "<localleader> c i") #'cider-inspect-last-result)
-  (evil-global-set-key 'normal (kbd "<localleader> c r") #'cider-interrupt)
-  (evil-global-set-key 'normal (kbd "<localleader> c n") #'cider-repl-set-ns))
+  (evil-define-key 'normal 'clojure-mode-map (kbd "<localleader> e r") #'cider-eval-defun-at-point)
+  (evil-define-key 'normal 'clojure-mode-map (kbd "<localleader> e e") #'cider-eval-last-sexp)
+  (evil-define-key 'normal 'clojure-mode-map (kbd "<localleader> e b") #'cider-eval-buffer)
+  (evil-define-key 'normal 'clojure-mode-map (kbd "<localleader> c c") #'cider-jack-in)
+  (evil-define-key 'normal 'clojure-mode-map (kbd "<localleader> c i") #'cider-inspect-last-result)
+  (evil-define-key 'normal 'clojure-mode-map (kbd "<localleader> c r") #'cider-interrupt)
+  (evil-define-key 'normal 'clojure-mode-map (kbd "<localleader> c n") #'cider-repl-set-ns))
 (add-hook 'clojure-mode-hook #'id/clojure-mode-hook)
+
+;; fix jump stack error
+;; NOTE: vim's go-to-definition binding comes from `evil-collection'
+(defun id/clj-to-definition-golang (orig-fun &rest args)
+  (evil--jumps-push)
+  (let ((res (apply orig-fun args)))
+    res))
+(advice-add 'cider-find-var :around #'id/clj-to-definition-golang)
 
 (with-eval-after-load 'clj-refactor
   ;; TODO(idanko): fix mapping
@@ -32,7 +40,7 @@
 (defun id/emacs-lisp-mode-hook ()
   (id/shared-lisp-mode-hook)
   (paredit-mode +1)
-  (evil-global-set-key 'normal (kbd "<localleader> e r") #'eval-defun)
-  (evil-global-set-key 'normal (kbd "<localleader> e e") #'eval-last-sexp)
-  (evil-global-set-key 'normal (kbd "<localleader> e b") #'eval-buffer))
+  (evil-define-key 'normal 'emacs-lisp-mode-map (kbd "<localleader> e r") #'eval-defun)
+  (evil-define-key 'normal 'emacs-lisp-mode-map (kbd "<localleader> e e") #'eval-last-sexp)
+  (evil-define-key 'normal 'emacs-lisp-mode-map (kbd "<localleader> e b") #'eval-buffer))
 (add-hook 'emacs-lisp-mode-hook #'id/emacs-lisp-mode-hook)
