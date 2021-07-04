110517739973870fca6ee327a94c8cbe0f3c3af2
Illia Danko <illia@aigent.com>
2021-07-04 19:38:10 +0300

    buffer: use C-/ to toggle other-window

--- cfg/buffer.el
+++ cfg/buffer.el
@@ -5,5 +5,12 @@
 (evil-global-set-key 'normal (kbd "<leader> b r") #'counsel-recentf)
 (evil-global-set-key 'normal "X" #'kill-this-buffer)
 
+;; XXX the idea is: if it's only one window duplicate it and switch, otherwise
+;; run delete-other-windows.
+(defun id/toggle-only-window ()
+  (interactive)
+  (delete-other-windows))
+(evil-global-set-key 'normal (kbd "C-/") #'id/toggle-only-window)
+
 (with-eval-after-load 'ivy
   (evil-global-set-key 'normal (kbd "<leader> <") #'ivy-switch-buffer))
