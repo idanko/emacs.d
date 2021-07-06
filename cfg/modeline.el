f2723e51def2ddc2022e23fee82d7cfe3c021d9d
Illia Danko <illia@aigent.com>
2021-07-06 22:34:45 +0300

    switch to doom-modeline

--- cfg/modeline.el
+++ cfg/modeline.el
@@ -1,17 +1,5 @@
 ;;; ~/.emacs.d/cfg/modeline.el -*- lexical-binding: t; -*-
 
-(defvar modeline-minor-white-list '(flycheck-mode)
-  "The whitelist of the minor modes being shown on the modeline.")
+(straight-use-package 'doom-modeline)
+(doom-modeline-mode +1)
 
-;;; Turn off minor modes.
-;; https://emacs.stackexchange.com/questions/3925/hide-list-of-minor-modes-in-mode-line#answer-3928
-(defun purge-minor-modes ()
-  (interactive)
-  (setq minor-mode-alist
-        (mapcar (lambda (x)
-                  (if (member (car x) modeline-minor-white-list)
-                      x
-                    `(,(car x) "")))
-                minor-mode-alist)))
-
-(add-hook 'after-change-major-mode-hook 'purge-minor-modes)
